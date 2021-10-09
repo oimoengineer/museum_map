7ac6052fef2436d5e98750b576e2865c3c806650
imoco <oimoengineer0224@gmail.com>
2021-10-10 01:11:32 +0900

    master

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,8 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
   <title>Museum Nav</title>
   <script src="{{ asset('js/app.js')}}" defer></script>
   
