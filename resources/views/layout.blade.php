2fa3fe83a3ba80e23a5a06c644d3c6c25e7fa75b
imoco <oimoengineer0224@gmail.com>
2021-10-09 20:41:12 +0900

    master

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="{{asset('/css/style.css')}}">
-  <title>Museum Map</title>
+  <title>Museum Nav</title>
   <script src="{{ asset('js/app.js')}}" defer></script>
   
 </head>
@@ -15,7 +15,7 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm fixed-top">
             <div class="container">
                 <a class="navbar-brand fs-3" href="{{ url('/') }}">
-                    {{ config('APP_NAME', 'Museum Map') }}
+                    {{ config('APP_NAME', 'Museum Nav') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
