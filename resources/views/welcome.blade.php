eeaa4b77bcc4236d58f74e0d8114a4b147e0d3a5
imoco <oimoengineer0224@gmail.com>
2021-10-11 01:03:07 +0900

    master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,27 @@
-@extends('layout')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+  <title>Museum Nav</title>
+  
+</head>
+<body class='main'>
+            <div class="d-flex" id="welcome">
+                <div class="content">
+                    <h1>Museum Nav</h1>
+                    <p>美術館をもっと身近に</p>
+                </div><!-- /.content -->
+                <div class="key_visual">
+                    <img src="{{asset('images/key_visual.png')}}" alt="メイン画像">
+                </div><!-- /.key_visual -->
+            </div><!-- /.d-flex -->
 
-@section('content')
-            <div class="content">
-                <div class="title m-b-md">
-                    Museum Nav
-                </div>
-            </div>
-@endsection
-
+</body>
+</html>
