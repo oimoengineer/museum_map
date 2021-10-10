6dfbec39b90574a617417a84bc2c369e28afc276
imoco <oimoengineer0224@gmail.com>
2021-10-10 20:35:33 +0900

    master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,32 @@
 @extends('layout')
 
 @section('content')
-  <div class="mt-2 mb-2 d-flex bd-highlight">
-    <h2 class='p-2 bd-highlight'>おすすめ一覧</h2>
-    <ul class='nav p-2 bd-highlight fs-5 mt-2'>
-      <li class='nav-item'><a class='text-decoration-none me-3 ms-5' href="#m1">美術館</a></li>
-      <li class='nav-item'><a class='text-decoration-none me-3' href="#m2">博物館</a></li>
-      <li class='nav-item'><a class='text-decoration-none' href="#m3">ギャラリー</a></li>
-    </ul> 
-    <form action="{{url('/search')}}" method="post" class="ms-auto p-2 bd-highlight">
-    {{ csrf_field()}}
-    {{method_field('get')}}
-    <div class="d-flex">
-    <div class="form-group">
-      <label>検索</label>
-      <input type="text" class="form-control col-md-5" placeholder="美術館・博物館・ギャラリー名を入力" name="name">
-    </div>
-    <button type="submit" class='btn btn-primary col-md-5 ms-3 align-self-end'>検索</button>
+<div class="position-relative">
+  <form action="{{url('/search')}}" method="post" class="ms-auto p-2 bd-highlight position-absolute end-0">
+      {{ csrf_field()}}
+      {{method_field('get')}}
+      <div class="d-flex">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="美術館・博物館・ギャラリー名を入力" name="name">
+      </div>
+      <button type="submit" class='btn btn-dark ms-3 align-self-end'>検索</button>
     </div><!-- /.d-flex -->
-    </form>
+  </form>
+</div><!-- /.position-relative -->
+  <div class="mb-4 bd-highlight">
+    <div class="d-flex">
+      <h2 class='p-2 bd-highlight text-dark'>おすすめ一覧</h2>
+      <div class="d-flex">
+        <ul class='nav p-2 bd-highlight fs-5 mt-2 ms-3'>
+          <li class='nav-item'><a class='text-decoration-none me-3' href="#m1">美術館</a></li>
+          <li class='nav-item'><a class='text-decoration-none me-3' href="#m2">博物館</a></li>
+          <li class='nav-item'><a class='text-decoration-none' href="#m3">ギャラリー</a></li>
+        </ul> 
+    </div><!-- /.d-flex -->
   </div><!-- /.d-flex -->
   
   <!-- 美術館 -->
-  <p id='m1'>美術館</p>
+  <p id='m1' class='border-start border-dark border-4 fs-4 ps-3 text-dark mt-4'>美術館</p>
   <div class="row">
     @foreach ($museums as $museum)
     @if($museum->category_id === 1)
@@ -36,10 +40,10 @@
       <div class="card-body">
         <h5 class="card-title">{{ $museum->name }}</h5>
         <p class="card-text">
-          住所<br>
+          <span>住所</span><br>
           {{ $museum->address }}</p>
-          <p class="card-text">投稿者：{{ $museum->user->name }}</p>
-        <p class="card-text">カテゴリ：{{ $museum->category->name }}</p>
+          <p class="card-text"><span>投稿者</span>{{ $museum->user->name }}</p>
+        <p class="card-text"><span>カテゴリ</span>{{ $museum->category->name }}</p>
         <a href={{ route('museum.detail', ['id' => $museum->id]) }} class="btn btn-primary">詳細を見る</a>
       </div>
       </div><!-- /.col-sm-6 -->
@@ -49,7 +53,7 @@
 </div><!-- /.row -->
 
 <!-- 博物館 -->
-<p id='m2'>博物館</p>
+<p id='m2' class='border-start border-dark border-4 fs-4 ps-3 text-dark mt-5'>博物館</p>
   <div class="row">
     @foreach ($museums as $museum)
     @if($museum->category_id === 2)
@@ -64,10 +68,10 @@
       <div class="card-body">
         <h5 class="card-title">{{ $museum->name }}</h5>
         <p class="card-text">
-          住所<br>
+        <span>住所</span><br>
           {{ $museum->address }}</p>
-          <p class="card-text">投稿者：{{ $museum->user->name }}</p>
-        <p class="card-text">カテゴリ：{{ $museum->category->name }}</p>
+          <p class="card-text"><span>投稿者</span>{{ $museum->user->name }}</p>
+        <p class="card-text"><span>カテゴリ</span>{{ $museum->category->name }}</p>
         <a href={{ route('museum.detail', ['id' => $museum->id]) }} class="btn btn-primary">詳細を見る</a>
       </div>
       </div><!-- /.col-sm-6 -->
@@ -77,7 +81,7 @@
 </div><!-- /.row -->
   
 <!-- ギャラリー -->
-<p id='m3'>ギャラリー</p>
+<p id='m3' class='border-start border-dark border-4 fs-4 ps-3 text-dark mt-5'>ギャラリー</p>
   <div class="row">
     @foreach ($museums as $museum)
     @if($museum->category_id === 3)
@@ -92,11 +96,11 @@
       <div class="card-body">
         <h5 class="card-title">{{ $museum->name }}</h5>
         <p class="card-text">
-          住所<br>
+        <span>住所</span><br>
           {{ $museum->address }}</p>
-          <p class="card-text">投稿者：{{ $museum->user->name }}</p>
-        <p class="card-text">カテゴリ：{{ $museum->category->name }}</p>
-        <a href={{ route('museum.detail', ['id' => $museum->id]) }} class="btn btn-primary">詳細を見る</a>
+          <p class="card-text"><span>投稿者</span>{{ $museum->user->name }}</p>
+        <p class="card-text"><span>カテゴリ</span>{{ $museum->category->name }}</p>
+        <a href={{ route('museum.detail', ['id' => $museum->id]) }} class="btn btn-outline-dark">詳細を見る</a>
       </div>
       </div><!-- /.col-sm-6 -->
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,10 @@
 @extends('layout')
 
 @section('content')
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/') }}">ホーム</a>
-                    @else
-                        <a href="{{ route('login') }}">ログイン</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">新規登録</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Museum Nav
                 </div>
             </div>
-        </div>
 @endsection
 
