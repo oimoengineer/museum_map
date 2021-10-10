2b9a280fd9d1ac653ef14d7200762eff33e7c7e6
imoco <oimoengineer0224@gmail.com>
2021-10-10 22:50:43 +0900

    master

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -49,21 +49,21 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('user.setting') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('setting');">
+                                    <a class="dropdown-item" href={{ route('user.setting') }}>
                                         マイページ
                                     </a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a class="dropdown-item" href={{ route('logout') }}
                                        onclick="event.preventDefault();
                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     
-                                    <form id="setting" action="{{ route('user.setting') }}" method="GET" style="display: none;">
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action={{ route('logout') }} method="POST" style="display: none;">
                                         @csrf
                                     </form>
+                                    
+
+                                        
                                 </div>
                             </li>
                         @endguest
