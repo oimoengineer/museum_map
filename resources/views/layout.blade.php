a04b59e803aebe06889f0e7a8d217621affdf091
imoco <oimoengineer0224@gmail.com>
2021-10-16 19:38:35 +0900

    master

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <meta name="description" content="おすすめの美術館をシェアするアプリ。あなたのお気に入りの美術館・博物館・ギャラリーを紹介しませんか？※機能は随時拡張予定です。">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
