f8793770f7bcbcc15d077cbb527c54067b53a3e8
Nomi <oimoengineer0224@gmail.com>
2021-11-21 17:32:53 +0900

    master

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['id'];
+
+    public function create() {
+        Category::create([
+            'id' => 1,
+            'name' => '美術館',
+        ]);
+    }
 }
