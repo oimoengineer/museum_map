7058db43d4fcf0dbc3305dcf3a37ef3784e53cbd
Nomi <oimoengineer0224@gmail.com>
2021-12-09 10:38:25 +0900

    artizon

--- public/js/result.js
+++ public/js/result.js
@@ -1,9 +1,11 @@
 // GoogleMap API用の initMap関数を作成
 function initMap() {
   map = document.getElementById("map");
+  let artizon = {lat: 35.67905925545413, lng:139.7716643841635};
 
   opt = {
-    zoom: 13
+    zoom: 13,
+    center: artizon,
   };
   // 地図のインスタンスを作成
   mapObj = new google.maps.Map(map, opt);
